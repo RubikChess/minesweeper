faa708cb7a6bcc686449a39e011aa56dbd31bb5a
RubikChess <johndoe49@5x2.de>
2020-06-01 20:02:49 +0200

    added function to place randomly bombs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from field import *
+from random import randint
 
 
 class App:
@@ -14,11 +15,25 @@ class App:
         self.fields = [[Field(x, y) for x in range(self.columns)]
                        for y in range(self.rows)]
 
+        self.place_bombs()
+
         self.printBoardButton = Button(
             frame, text="print board", command=self.print_board)
         self.printBoardButton.pack(side=LEFT)
 
+    def place_bombs(self):
+        for i in range(self.number_of_bombs):
+            self.place_bomb()
+
+    def place_bomb(self):
+        x = randint(0, self.columns - 1)
+        y = randint(0, self.rows - 1)
+
+        self.fields[x][y].hasBomb = True
+
     def print_board(self):
+        print("Field:")
+
         for column in range(self.columns):
             rowRepresentation = ""
             for row in range(self.rows):
