d821bdf0acf2ba8c806d6a4420eec95dc26d4e5c
RubikChess <johndoe49@5x2.de>
2020-06-01 19:46:29 +0200

    added pretty print of board on console

--- src/field.py
+++ src/field.py
@@ -2,9 +2,11 @@ class Field:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.hasBomb = False
+        self.isDiscovered = False
 
     def show(self):
-        return "Field[x:"+str(self.x)+", y"+str(self.y)+"]"
+        return "Field[x:" + str(self.x)+", y: " + str(self.y) + ", hasBomb: " + str(self.hasBomb) + ", isDiscovered: " + str(self.isDiscovered) + "]"
 
     def __repr__(self):
         return self.show()
--- src/main.py
+++ src/main.py
@@ -4,18 +4,34 @@ from field import *
 
 class App:
     columns, rows = 10, 10
+    number_of_bombs = 10
+    bomb_fields = []
 
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
 
-        self.fields = [[Field(x,y) for x in range(self.columns)] for y in range(self.rows)]
+        self.fields = [[Field(x, y) for x in range(self.columns)]
+                       for y in range(self.rows)]
 
-        self.printBoardButton = Button(frame, text="print board", command=self.printBoard)
+        self.printBoardButton = Button(
+            frame, text="print board", command=self.print_board)
         self.printBoardButton.pack(side=LEFT)
 
-    def printBoard(self):
-        print(self.fields)
+    def print_board(self):
+        for column in range(self.columns):
+            rowRepresentation = ""
+            for row in range(self.rows):
+                rowRepresentation = rowRepresentation + " " + \
+                    self.show_field(self.fields[column][row])
+
+            print(str(column) + " " + rowRepresentation)
+
+    def show_field(self, field):
+        if field.hasBomb:
+            return "x"
+        else:
+            return "o"
 
 
 root = Tk()
